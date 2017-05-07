58dcfdb1d1a4b53211521358b4c061267816a2a7
Microeinstein <m1cr0th3bes7@gmail.com>
2017-05-07 17:42:58 +0200

    Aggiungere i file di progetto.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("ThreadTimer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microeinstein")]
+[assembly: AssemblyProduct("ThreadTimer")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili 
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da 
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("95119619-90ec-41bb-bae8-afb0c5b38301")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria 
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Timer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace Micro.ThreadTimer {
+    public class Timer {
+        private BackgroundWorker backWork;
+        private bool runn;
+        public event Action Tick;
+
+        public bool Running {
+            get { return runn; }
+            set {
+                if (value != runn) {
+                    if (value) {
+                        backWork = new BackgroundWorker();
+                        backWork.WorkerSupportsCancellation = true;
+                        backWork.DoWork += new DoWorkEventHandler(background);
+                        backWork.RunWorkerAsync();
+                    } else {
+                        backWork.CancelAsync();
+                        backWork.Dispose();
+                        backWork = null;
+                    }
+                    runn = value;
+                }
+            }
+        }
+        public int Intervall { get; set; }
+
+        public Timer(int intervall, bool running = false) {
+            Intervall = intervall;
+            Running = running;
+        }
+        public void start() {
+            Running = true;
+        }
+        public void stop() {
+            Running = false;
+        }
+
+        private void background(object sender, DoWorkEventArgs e) {
+            while (!e.Cancel) {
+                Thread.Sleep(Intervall);
+                Tick();
+            }
+        }
+    }
+}
